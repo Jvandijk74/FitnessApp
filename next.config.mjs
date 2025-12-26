de1a15f17e9feb9529672cb506e82666e1c18975
Jesse van Dijk <jessevandijk@mac.home>
2025-12-26 21:47:18 +0100

    Fix duplicate next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
-  images: {
-    remotePatterns: [],
-  },
-};
-
-export default nextConfig;/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-    serverActions: true
-  },
-  images: {
-    remotePatterns: []
-  }
 };
 
 export default nextConfig;
